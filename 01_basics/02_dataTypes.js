0b10ea168efb3fa17aee46efc83e405afbe1ddd9
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-09 14:41:09 +0000

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS Code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+console.log(3
+    + 3
+)   // code readability should be high
+
+console.log("Sakshi")
+
+let name = "sakshi"
+let age = 19
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/ false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+console.log(typeof age);  
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
