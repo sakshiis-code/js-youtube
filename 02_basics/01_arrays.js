cfbfaf5cb4329ee37b87abdcfa9d37d81c95533a
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-11 10:07:16 +0000

    Some changes

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -26,13 +26,13 @@ const newArr = myArr.join()
  
 // slice, splice
 
-console.log("A", myArr);
+// console.log("A", myArr);
 const myn1 = myArr.slice(1,3)
-console.log(myn1);
-console.log("B", myArr);
+// console.log(myn1);
+// console.log("B", myArr);
 
 const myn2 = myArr.splice(1, 3)
-console.log("C", myArr);
-console.log(myn2);
+// console.log("C", myArr);
+// console.log(myn2);
 
 
