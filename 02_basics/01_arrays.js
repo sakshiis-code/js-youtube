a03798146dc629953e3e3fffcbde195224bad7ec
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-11 05:24:00 +0000

    Array methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,14 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const makeupItems = ["Concealer", "Blush", "Foundation"]
+
+const giArr = new Array(30, 15, 20)
+console.log(myArr[1]);
+
+// Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+console.log(myArr);
