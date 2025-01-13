c723971d88d72411bf7ffb919503785ab36ca7d2
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-13 02:09:08 +0000

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,9 +4,48 @@ if (true) {
     let a = 10  // local scope
     const b = 20
     // c  = 30
-    console.log("Inner: ", a);    
+    // console.log("Inner: ", a);    
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "sarah"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);        
+    }
+
+    // console.log(website);
+    
+    two()
+}
+// one()
+
+if (true) {
+    const username = "sammy"
+    if (username === "sammy") {
+        const website = " youtube"
+        // console.log(username + website);        
+    }
+
+    // console.log(website);    
+}
+
+// console.log(username);
+
+
+console.log(addOne(5));
+function addOne(num) {
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num) {
+    return num + 2
+}
+console.log(addTwo(5));
