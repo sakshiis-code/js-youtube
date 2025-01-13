f3cbfdf8d2ef21f749f46927468cd9fde998e7ec
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-13 05:25:29 +0000

    How does Js execute code + call stack

---
+++ 03_basics/Js_notes.js
@@ -0,0 +1,5 @@
+// How does JavaScript execute code
+// https://excalidraw.com/#json=Ax0dQdE0hhcggzwXoWmWf,Rbe83Zwq9Xg8csAGamirfw
+
+// Call stack
+// https://excalidraw.com/#json=nnfvi8wvc2aw3FCmEuN_U,3pVqiXCDvsstBN2OhNpazA
