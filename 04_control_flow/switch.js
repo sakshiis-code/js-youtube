2c4642a08e1fff0c6079554e29c1af2052c38315
sakshiis-code <sakshihihisharma@gmail.com>
2025-01-14 04:13:31 +0000

    Control Flow in Js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,81 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 10
+switch (month) {
+    case 1:
+        console.log("January");        
+        break;
+    case 2:
+        console.log("February");        
+        break;
+    case 3:
+        console.log("March");        
+        break;
+    case 4:
+        console.log("April");        
+        break;
+    case 5:
+        console.log("May");        
+        break;
+    case 6:
+        console.log("June");        
+        break;
+    case 7:
+        console.log("July");        
+        break;
+    case 8:
+        console.log("August");        
+        break;
+    case 9:
+        console.log("September");        
+        break;
+    case 10:
+        console.log("October");        
+        break;
+    case 11:
+        console.log("November");        
+        break;
+    case 12:
+        console.log("December");        
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+const day = "Sum"
+switch (day) {
+    case "Mon":
+        console.log("Monday");
+        break;
+    case "Tue":
+        console.log("Tuesday");
+        break;
+    case "Wed":
+        console.log("Wednesday");
+        break;
+    case "Thurs":
+        console.log("Thursday");
+        break;
+    case "Fri":
+        console.log("Friday");
+        break;
+    case "Sat":
+        console.log("Saturday");
+        break;
+    case "Sun":
+        console.log("Sunday");
+        break;
+
+    default:
+        console.log("Invalid day");
+        break;
+}
